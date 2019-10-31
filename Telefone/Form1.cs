e10109585e0ee5fbc6c8772dc65c2007c297b29f
Kintas17 <49915294+Kintas17@users.noreply.github.com>
2019-10-31 10:51:33 +0000

    Add files via upload

---
+++ Telefone/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Telefone
+{
+    public partial class Form1 : Form
+    {
+
+        int numero;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mostra()
+        {
+            Console.Beep();
+            label1.Text = numero.ToString();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int valor = 0;
+            switch (((Button)sender).Name)
+            {
+                case "button1": valor = 1; break;
+                case "button2": valor = 2; break;
+                case "button3": valor = 3; break;
+                case "button4": valor = 4; break;
+                case "button5": valor = 5; break;
+                case "button6": valor = 6; break;
+                case "button7": valor = 7; break;
+                case "button8": valor = 8; break;
+                case "button9": valor = 9; break;
+                case "button10": valor = 0; break;
+            }
+            if (numero.ToString().Length < 9)
+                numero = numero * 10 + valor;
+            mostra();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            numero = 0;
+        }
+
+        private void Button12_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            numero = 0;
+            mostra();
+        }
+    }
+}
